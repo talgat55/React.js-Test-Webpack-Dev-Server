fbe78d0eee5f904dbde347d03da76abc6b4032f3
talgat55 <lightx_design@yahoo.com>
2016-11-11 14:31:45 +0600

    init

---
+++ app/app.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Index from './components/Index.jsx';
+
+ReactDOM.render(
+    <Index />,
+    document.getElementById('root')
+);
---
+++ app/components/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+
+var Index = React.createClass({
+    render: function() { 
+
+        return (
+            <div className="note">
+                Hello World
+            </div>
+        );
+    }
+});
+
+module.exports = Index;
